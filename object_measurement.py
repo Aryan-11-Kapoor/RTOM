3aea0281b82d23c68563aeb5d467ab5f133d0901
Aryan-11-Kapoor <76451992+Aryan-11-Kapoor@users.noreply.github.com>
2022-07-05 20:19:40 +0530

    Add files via upload

--- object_measurement.py
+++ object_measurement.py
@@ -8,17 +8,22 @@ cap=cv2.VideoCapture(0)
 cap.set(10,160)
 cap.set(3,1920)
 cap.set(4,1080)
+scale = 3
+wP = 66*scale
+hP = 132*scale
+
 
 while True:
     if webcam: success,img = cap.read()
     else: img = cv2.imread(path)
 
-    img, conts = utlis.getContours(img,showCanny=True,minArea= 50000,filter=4)
+    img, conts = utlis.getContours(img,minArea= 50000,filter=4)
 
     if len(conts)!= 0:
         biggest = conts[0][2]
         #print(biggest)
-        utlis.warpImg(img, biggest, 100,100)
+        imgWarp = utlis.warpImg(img, biggest, wP, hP)
+        cv2.imshow('Phone', imgWarp)
 
     img = cv2.resize(img,(0,0),None,0.5,0.5)
     cv2.imshow('Original',img)
