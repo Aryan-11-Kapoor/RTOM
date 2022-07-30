bb3509cd2209093971108afd2b4d576ca55d1259
Bharath <bharathbdh65@gmail.com>
2022-07-30 22:52:16 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -5,15 +5,13 @@ st.image('https://miro.medium.com/max/568/1*Y1S4hciQTfrB3xJuk2remA.png')
 st.title("Real Time Object Measurement App")
 st.subheader("This app allows you to measure objects in real-time !!!")
 st.text("We use OpenCV and Streamlit for this demo")
-sideb = st.sidebar
-check1 = sideb.button("Live")
-check2 = sideb.button("Static")
-if check1:
-    try:
+options = st.sidebar.radio("select",('Real','Static'))
+if options == 'Real':
+    try:    
         live()
     except:
         pass
-elif check2:
+elif options == 'Static':
     try:
         static()
     except:
