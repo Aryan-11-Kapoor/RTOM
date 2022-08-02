39d8462d492a3d4864daf50c3dde41b944bc84d9
Aryan-11-Kapoor <76451992+Aryan-11-Kapoor@users.noreply.github.com>
2022-08-03 02:21:48 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -89,7 +89,7 @@ elif options == 'Static':
         with st.sidebar:
             with st.expander("INSTRUCTIONS TO USE"):
                 
-                st.info("""While takingapicture make sure to place your reference object on the left most part of your picture.
+                st.info("""While taking a picture make sure to place your reference object on the left most part of your picture.
                         Your reference object can be anything of your choice,provided you know the width of the said object.""")
                 st.image("prop.jpeg",caption='Coin in this particular case')
                 st.warning('Width of the reference object is in INCHES ')
